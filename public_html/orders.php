e75b9210b322dfbbc81b7279b39d43920edcb2a1
Denis_Kambolin <kambolin2001@yandex.ru>
2022-11-18 09:08:04 +0300

    Вносит правки в контекст

--- public_html/orders.php
+++ public_html/orders.php
@@ -2,36 +2,42 @@
     $orders = [
         'orders' =>[
             [
-                'class' => '"order"',
+                'class' => 'order',
                 'title' => 'Basic plan',
                 'logo' => '/image/card-black.png',
-                'price_m' => '$19',
-                'price_y' => '$199',
-                'button_class' => '"order__button"',
+                'price_month' => '$19',
+                'price_year' => '$199',
+                'line_class' => 'order__line',
+                'li_class' => 'order__includes-marker-list-item',
+                'button_class' => 'order__button',
                 'button_title' => 'Get started',
-                'includes_class' => '"order__includes"',
+                'includes_class' => 'order__includes',
                 'includes' => array('Vestibulum posuere odio','Id purus maximus condimentum','Vel convallis nibh duis',)
             ],
             [
-                'class' => '"order order_main"',
+                'class' => 'order order_main',
                 'title' => 'Business plan',
                 'logo' => '/image/card-green.png',
-                'price_m' => '$29',
-                'price_y' => '$299',
-                'button_class' => '"order__button order__button_main"',
+                'price_month' => '$29',
+                'price_year' => '$299',
+                'line_class' => 'order__line order__line_main',
+                'li_class' => 'order__includes-marker-list-item order__includes-marker-list-item_main',
+                'button_class' => 'order__button order__button_main',
                 'button_title' => 'Get started',
-                'includes_class' => '"order__includes order__includes_main"',
+                'includes_class' => 'order__includes order__includes_main',
                 'includes' => array('Maximus condimentum turpis','Nullam faucibus dictum accumsan','Vivamus ante dolor','Purus in convallis pharetra')
             ],
             [
-                'class' => '"order"',
+                'class' => 'order',
                 'title' => 'Enterprise plan',
                 'logo' => '/image/card-black.png',
-                'price_m' => '$49',
-                'price_y' => '$499',
-                'button_class' => '"order__button"',
+                'price_month' => '$49',
+                'price_year' => '$499',
+                'line_class' => 'order__line',
+                'li_class' => 'order__includes-marker-list-item',
+                'button_class' => 'order__button',
                 'button_title' => 'Get started',
-                'includes_class' => '"order__includes"',
+                'includes_class' => 'order__includes',
                 'includes' => array('Phasellus finibus lectus','At blandit mollis maecenas','Nec ultrices lectus eune','Mattis facilisis lacus aliquet','Am condimentum cursus',)
             ]
         ]
